38718c9da84edd2ea1de947f6c9988a838b31abe
CoderHook <Abel1987@hotmail.es>
2016-07-21 12:58:42 +0000

    changed the db user and pass

--- mongoutil.js
+++ mongoutil.js
@@ -6,7 +6,7 @@ let _db;
 
 module.exports = {
     connect(){
-        client.connect('mongodb://usertest:test@ds023325.mlab.com:23325/mysearch', (err, db)=>{
+        client.connect('mongodb://test:test@ds023325.mlab.com:23325/mysearch', (err, db)=>{
             
             if(err){
                 console.log("Error connection to mongo - check mongod connection");
